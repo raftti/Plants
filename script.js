d0eea0a1e5d85c40d94da2b1cf242f9ac1650ba4
raftti <raftti279@gmail.com>
2023-03-22 22:04:46 +0400

    init progect

---
+++ script.js
@@ -0,0 +1,26 @@
+const burger_btn = document.getElementById('burger_btn')
+const welcome_btn = document.getElementById('welcome_btn')
+const navbar__list = document.getElementById('navbar__list')
+const ser = document.querySelector('.navbar__right-part')
+const burgerMenu2 = document.querySelector('.navbar__list')
+
+
+
+burger_btn.addEventListener('click', toggleMenu)
+function toggleMenu() {
+    if(burger_btn.getAttribute('src') == '/sourses/images/burger.png') {
+        burger_btn.setAttribute('src', '/sourses/images/Arrow-PNG-Image.png')
+        burger_btn.style.marginLeft = '7px'
+        burger_btn.style.marginTop = '4px'
+        burger_btn.style.width = "25px"
+    }
+    else {
+        burger_btn.setAttribute('src', '/sourses/images/burger.png')
+        burger_btn.style.width = "40px"
+        burger_btn.style.marginLeft = '0px'
+        burger_btn.style.marginTop = '0px'
+    }
+    burgerMenu1.classList.toggle('open')
+    burgerMenu2.classList.toggle('open')
+}
+
