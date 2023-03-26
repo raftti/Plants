7db57306a63a6ac6135029f3b41d0e47c515a7b3
raftti <raftti279@gmail.com>
2023-03-26 23:02:19 +0400

    3update

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const burger_btn = document.getElementById('burger_btn')
 const welcome_btn = document.getElementById('welcome_btn')
 const navbar__list = document.getElementById('navbar__list')
-const ser = document.querySelector('.navbar__right-part')
 const burgerMenu1 = document.querySelector('.navbar__right-part')
-const burgerMenu2 = document.querySelector('.navbar__list')
 
+const cards = document.querySelectorAll('.service__item')
+const filterBtn = document.querySelectorAll('.service__btn')
 
 
 burger_btn.addEventListener('click', toggleMenu)
 function toggleMenu() {
-
     if(burger_btn.getAttribute('src') == '/sourses/images/burger.png') {
         burger_btn.setAttribute('src', '/sourses/images/Arrow-PNG-Image.png')
         burger_btn.style.marginLeft = '7px'
@@ -23,6 +22,19 @@ function toggleMenu() {
         burger_btn.style.marginTop = '0px'
     }
     burgerMenu1.classList.toggle('open')
-    burgerMenu2.classList.toggle('open')
+    navbar__list.classList.toggle('open')
 }
 
+
+
+
+filterBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+      let type = btn.dataset.type
+      console.log(type)
+      cards.forEach(card => {
+        card.classList.remove('blur')
+        if(card.dataset.type !== type) card.classList.add('blur')
+      }) 
+    }) 
+  })
